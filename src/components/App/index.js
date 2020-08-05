8cbf5ab218dcc02bca43cc3c09d0996a162cacab
Andrius <andrius.rochalazarino@gmail.com>
2020-08-05 13:21:31 -0400

    Estrutura de pastas completa e styled-components instalado

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { Store } from './store';
 function App() {
   return (
-    <Provider store={Store}>
       <div>
         Teste
       </div>
-    </Provider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { Store } from './store';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={Store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
