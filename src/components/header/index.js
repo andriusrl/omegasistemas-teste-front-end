27028f57a2c65fa81aa2d862e9caa64603a739c1
Andrius <andrius.rochalazarino@gmail.com>
2020-08-06 21:47:07 -0400

    Menu adicionado semm links ainda

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
 function Headers() {
   return (
-      <div>
-        teste app
-      </div>
+    <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit">
+            Bolsa Familia
+          </Typography>
+        </Toolbar>
+      </AppBar>
   );
 }
 
